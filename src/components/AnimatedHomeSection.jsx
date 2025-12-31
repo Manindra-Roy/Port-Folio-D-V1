dd3d3bd6b660490627d5a7c8fb05e2e0f8ac2883
Manindra Roy <anonymous.secret.in@gmail.com>
2025-12-31 14:05:02 +0530

    clock responsive update for width 360

--- src/components/AnimatedHomeSection.jsx
+++ src/components/AnimatedHomeSection.jsx
@@ -50,7 +50,7 @@ const AnimatedHomeSection = ({
     );
   }, []);
   return (
-    <div ref={contextRef} style={{height:"50%"}}>
+    <div ref={contextRef} style={{height:"55%"}}>
       <div style={{ clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)" }}>
         <div
           ref={headerRef}
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -71,7 +71,7 @@ results driven webs/apps`
       />
       <figure
         className="absolute inset-0 -z-20"
-        style={{ width: "100dvw", height: "50%",  }}
+        style={{ width: "100dvw", height: "45%",  }}
       >
         <Canvas
           // 2. PAUSE RENDERING when out of view
